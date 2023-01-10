b7384fa24c3789dad0dfaeec2feba1b9b4d574ba
AKiroi <alexandr.kiroi@gmail.com>
2023-01-10 17:45:10 +0200

    feat: add cartItem funcianality

--- src/pages/cartPage/CartPage.ts
+++ src/pages/cartPage/CartPage.ts
@@ -41,29 +41,36 @@ class CartPage {
   //  return Math.ceil(this.cartItems.length / +this.cardItemValue);
   //}
 
-  handlerCart = () => {
+  handlerCart = (e: Event) => {
     const cartHeaderItem = document.querySelector('.cart__header-item') as HTMLInputElement;
     const cartPagesArrowLeft = document.querySelector('.cart__pages-arrow-left');
     const cartPagesArrowRight = document.querySelector('.cart__pages-arrow-right');
     const cartPagesCount = document.querySelector('.cart__pages-count');
     const cartItems = document.querySelector('.cart__items');
-    const cartItem = document.querySelector('.cart__item');
+    const cartItem = document.querySelectorAll('.cart__item');
     const plusIcon = document.querySelector('.plus-icon');
     const minusIcon = document.querySelector('.minus-icon');
     const cartAddCount = document.querySelector('.cart__add-count');
+
+    //const target = e.target!;
+    //console.log(target.closest('.cards__item'));
+
+    //console.log();
+    //cartItems?.addEventListener('click', (e: Event) => {
+
+    //})
+
+    //cartItem.forEach((item) => {
+    //  item.addEventListener('click', (e) => {
+    //    console.log(plusIcon);
+
+    //  })
+    //})
 
     const countCartItems = cartHeaderItem.value;
     const countPages = Math.ceil(this.cartItems.length / +countCartItems);
 
     cartHeaderItem.addEventListener('input', () => {
-      //this.generateCardCurrent(countCartItems, countPages)
-      //let arr = []
-      //for (let i = 0; i < countPages; i++) {
-      //  let arrItems = []
-      //  for (let i = 0; i < +countCartItems; i++) {
-      //    if (cartItems.leng)
-      //  }
-      //}
       const carts = [...this.cartItems];
       let arrCarts = [];
       for (let i = 0; i < countPages; i++) {
@@ -124,7 +131,6 @@ class CartPage {
       const cartPagesArrowRight = document.querySelector('.cart__pages-arrow-right');
       const countPages = Math.ceil(this.cartItems.length / +cartHeaderItem.value);
 
-
       const generateCardCurrent = () => {
         const carts = [...this.cartItems];
         let arrCarts = [];
@@ -148,10 +154,11 @@ class CartPage {
       cartHeader.append(cartHeaderTitle, cartHeaderBox);
 
       let currentPage = 1;
-      let index = 1
+      let index = 1;
 
       const createCartItems = (item: Igoods) => {
         const cartItem = createHTMLElement('cart__item');
+        cartItem.dataset.id = item.id.toString();
 
         cartItem.innerHTML = `
             <div class="cart__right">
@@ -170,22 +177,66 @@ class CartPage {
             <div class="cart__left">
               <div class="cart__add-item">
                 <div class="cart__plus">
-                  <img src="./assets/icons/plus-icon.svg" alt="plus-icon">
+                  <img class="cart__plus-item" src="./assets/icons/plus-icon.svg" alt="plus-icon">
                 </div>
                 <div class="cart__add-count">${item.count}</div>
                 <div class="cart__minus">
-                  <img src="./assets/icons/mines-icon.svg" alt="minus-icon">
+                  <img class="cart__minus-item" src="./assets/icons/mines-icon.svg" alt="minus-icon">
                 </div>
               </div>
               <div class="cart__price">${item.price}$</div>
             </div>
         `;
 
+        cartItem.addEventListener('click', (e: Event) => {
+          const target = e.target as HTMLElement;
+          const dataId = cartItem.getAttribute('data-id') as string;
+
+          const cartAddCount = document.querySelectorAll('.cart__add-count')!;
+          const cartPrice = document.querySelectorAll('.cart__price')!;
+          const cartTotalCount = document.querySelector('.cart__total-count')!;
+          const cartProductCount = document.querySelector('.cart__product-count')!;
+          const headerItemText = document.querySelector('.header__item-total')!;
+
+          this.cartItems.forEach((item, i) => {
+            if (item.id === +dataId) {
+              if (target.classList.contains('cart__plus-item')) {
+                item.count++;
+              }
+              if (target.classList.contains('cart__minus-item')) {
+                item.count--;
+                if (item.count === 0) {
+                  cartItem.remove();
+                }
+              }
+              cartAddCount[i].textContent = item.count.toString();
+              cartPrice[i].textContent = (item.count * item.price).toString() + '$';
+            }
+          });
+
+          this.cartItems = this.cartItems.filter((item) => item.count !== 0);
+
+          cartProductCount.textContent = getCartItemsCount().toString();
+          cartTotalCount.textContent = getCartTotalPrice().toString() + '$';
+          headerItemText.textContent = getCartTotalPrice().toString() + '$';
+        });
+
         return cartItem;
       };
 
       let cards = this.cartPages[currentPage - 1].map((item) => createCartItems(item));
 
+      const getCartItemsCount = () => {
+        return this.cartItems.reduce((acc, curr) => {
+          return acc + curr.count;
+        }, 0);
+      };
+
+      const getCartTotalPrice = () => {
+        return this.cartItems.reduce((acc, curr) => {
+          return acc + curr.price * curr.count;
+        }, 0)
+      };
 
       const cartFooter = createHTMLElement('cart__footer');
       const cartPromoBlock = createHTMLElement('cart__promo-block');
@@ -195,13 +246,9 @@ class CartPage {
       const cartPromoBye = createHTMLElement('cart__promo-byu');
       const cartProducts = createHTMLElement('cart__products');
       const cartProduct = createHTMLElement('cart__product');
-      cartProduct.innerHTML = `Products: <span class="cart__product-count">${this.cartItems.reduce((acc, curr) => {
-        return acc + curr.count;
-      }, 0)}</span>`;
+      cartProduct.innerHTML = `Products: <span class="cart__product-count">${getCartItemsCount()}</span>`;
       const cartTotal = createHTMLElement('cart__total');
-      cartTotal.innerHTML = `Total: <span class="cart__total-count">${this.cartItems.reduce((acc, curr) => {
-        return acc + curr.price * curr.count;
-      }, 0)}$</span>`;
+      cartTotal.innerHTML = `Total: <span class="cart__total-count">${getCartTotalPrice()}$</span>`;
       const btnSubmit = document.createElement('button') as HTMLButtonElement;
       btnSubmit.type = 'submit';
       btnSubmit.innerHTML = 'BUY NOW';
@@ -219,153 +266,6 @@ class CartPage {
       cartPromoBye.append(cartProducts, btnSubmit);
       cartFooter.append(cartPromoBlock, cartPromoBye);
       cartMainContainer.append(cartModalOverlay, cartHeader, cartMain, cartFooter, cartModalContainer);
-      // cartContainer.innerHTML = `
-      //   <div class="cart__container">
-      //     <div class="cart__header">
-      //       <div class="cart__header-title">Product in Cart</div>
-      //       <div class="cart__header-box">
-      //         <div class="cart__header-items">Items: <span class="cart__header-item">10</span></div>
-      //         <div class="cart__pages">
-      //           Page:
-      //           <img src="./assets/icons/arrow-left-icon.svg" alt="arrow-left icon" class="cart__pages-arrow-left">
-      //           <span class="cart__pages-count">1</span>
-      //           <img src="./assets/icons/arrow-right-icon.svg" alt="arrow-left icon" class="cart__pages-arrow-left">
-      //         </div>
-      //       </div>
-      //     </div>
-
-      //     <div class="cart__main">
-      //       <div class="cart__items">
-      //         <div class="cart__item">
-      //           <div class="cart__right">
-      //             <div class="cart__item-number">1</div>
-      //             <div class="cart__img-box">
-      //               <img class="cart__img" src="./assets/img/213669_1-1200x800.jpeg" alt="item picture">
-      //             </div>
-      //             <div class="cart__content">
-      //               <div class="cart__title">Blackstar ACOUSTIC</div>
-      //               <div class="cart__description">
-      //                 Brand: BLACKSTAR; Categoty: GUITAR CABINETS;
-      //                 Discont: 10.8; Rating: 4,8; InStock: 18;
-      //               </div>
-      //             </div>
-      //           </div>
-      //           <div class="cart__left">
-      //             <div class="cart__add-item">
-      //               <div class="cart__plus">
-      //                 <img src="./assets/icons/plus-icon.svg" alt="plus-icon">
-      //               </div>
-      //               <div class="cart__add-count">1</div>
-      //               <div class="cart__minus">
-      //                 <img src="./assets/icons/mines-icon.svg" alt="minus-icon">
-      //               </div>
-      //             </div>
-      //             <div class="cart__price">400$</div>
-      //           </div>
-      //         </div>
-
-      //         <div class="cart__item">
-      //           <div class="cart__right">
-      //             <div class="cart__item-number">1</div>
-      //             <div class="cart__img-box">
-      //               <img class="cart__img" src="./assets/img/213669_1-1200x800.jpeg" alt="item picture">
-      //             </div>
-      //             <div class="cart__content">
-      //               <div class="cart__title">Blackstar ACOUSTIC</div>
-      //               <div class="cart__description">
-      //                 Brand: BLACKSTAR; Categoty: GUITAR CABINETS;
-      //                 Discont: 10.8; Rating: 4,8; InStock: 18;
-      //               </div>
-      //             </div>
-      //           </div>
-      //           <div class="cart__left">
-      //             <div class="cart__add-item">
-      //               <div class="cart__plus">
-      //                 <img src="./assets/icons/plus-icon.svg" alt="plus-icon">
-      //               </div>
-      //               <div class="cart__add-count">1</div>
-      //               <div class="cart__minus">
-      //                 <img src="./assets/icons/mines-icon.svg" alt="minus-icon">
-      //               </div>
-      //             </div>
-      //             <div class="cart__price">400$</div>
-      //           </div>
-      //         </div>
-
-      //         <div class="cart__item">
-      //           <div class="cart__right">
-      //             <div class="cart__item-number">1</div>
-      //             <div class="cart__img-box">
-      //               <img class="cart__img" src="./assets/img/213669_1-1200x800.jpeg" alt="item picture">
-      //             </div>
-      //             <div class="cart__content">
-      //               <div class="cart__title">Blackstar ACOUSTIC</div>
-      //               <div class="cart__description">
-      //                 Brand: BLACKSTAR; Categoty: GUITAR CABINETS;
-      //                 Discont: 10.8; Rating: 4,8; InStock: 18;
-      //               </div>
-      //             </div>
-      //           </div>
-      //           <div class="cart__left">
-      //             <div class="cart__add-item">
-      //               <div class="cart__plus">
-      //                 <img src="./assets/icons/plus-icon.svg" alt="plus-icon">
-      //               </div>
-      //               <div class="cart__add-count">1</div>
-      //               <div class="cart__minus">
-      //                 <img src="./assets/icons/mines-icon.svg" alt="minus-icon">
-      //               </div>
-      //             </div>
-      //             <div class="cart__price">400$</div>
-      //           </div>
-      //         </div>
-
-      //         <div class="cart__item">
-      //           <div class="cart__right">
-      //             <div class="cart__item-number">1</div>
-      //             <div class="cart__img-box">
-      //               <img class="cart__img" src="./assets/img/213669_1-1200x800.jpeg" alt="item picture">
-      //             </div>
-      //             <div class="cart__content">
-      //               <div class="cart__title">Blackstar ACOUSTIC</div>
-      //               <div class="cart__description">
-      //                 Brand: BLACKSTAR; Categoty: GUITAR CABINETS;
-      //                 Discont: 10.8; Rating: 4,8; InStock: 18;
-      //               </div>
-      //             </div>
-      //           </div>
-      //           <div class="cart__left">
-      //             <div class="cart__add-item">
-      //               <div class="cart__plus">
-      //                 <img src="./assets/icons/plus-icon.svg" alt="plus-icon">
-      //               </div>
-      //               <div class="cart__add-count">1</div>
-      //               <div class="cart__minus">
-      //                 <img src="./assets/icons/mines-icon.svg" alt="minus-icon">
-      //               </div>
-      //             </div>
-      //             <div class="cart__price">400$</div>
-      //           </div>
-      //         </div>
-
-      //       </div>
-      //     </div>
-      //     <div class="cart__footer">
-      //       <div class="cart__promo-block">
-      //         <input class="cart__promo-input" type="text" placeholder="Enter promo code">
-      //         <div class="cart__promo-text">Promo for test: 'RS', 'EPM'</div>
-      //       </div>
-      //       <div class="cart__promo-byu">
-      //         <div class="cart__products">
-      //           <div class="cart__product">Products: <span class="cart__product-count">10</span></div>
-      //           <div class="cart__total">Total: <span class="cart__total-count">400$</span></div>
-      //         </div>
-      //         <a href="" class="cart__btn-byu">BUY NOW</a>
-      //       </div>
-      //     </div>
-
-      //   </div>
-      //   `;
 
       if (localStorage.getItem('modal')) {
         setTimeout(() => {
